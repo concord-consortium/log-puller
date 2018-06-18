6fc2119ef08c7751d0cc89b1d5e0ba74d2329b19
Doug Martin <doug@zoopdoop.com>
2018-06-18 09:40:13 -0400

    Added summary totals to wget-list

--- index.js
+++ index.js
@@ -279,7 +279,7 @@ var wgetList = function (req, res) {
 
   req.db(function (client, done) {
     client
-      .query("SELECT MIN(id) AS min_id, MAX(id) AS max_id FROM logs")
+      .query("SELECT COUNT(*) AS total, MIN(id) AS min_id, MAX(id) AS max_id FROM logs")
       .on('error', function (err) {
         done();
         res.error(err.toString());
@@ -288,6 +288,8 @@ var wgetList = function (req, res) {
         var minId = parseInt(row.min_id);
         var maxId = parseInt(row.max_id);
         var numRows = parseInt(req.query.num_rows) || 1000;
+
+        res.write('# count = ' + row.total + ' min_id = ' + row.min_id + ' max_id = ' + row.max_id + '\n');
 
         for (var i = minId; i < maxId; i += numRows) {
           res.write('wget -O ' + getDumpName(i) + " 'https://log-puller.herokuapp.com/dump?dump_key=" + dumpKey + '&start_row=' + i + '&num_rows=' + numRows + "'\n");
