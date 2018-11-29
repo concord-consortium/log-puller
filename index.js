40b6b416977eab7497736daae2585beb80f9312d
Doug Martin <doug@zoopdoop.com>
2018-11-29 16:24:03 -0500

    Extended body-parser limit to 50mb

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ var pool = new pg.Pool({
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true, limit: "50mb", parameterLimit:50000}));
+app.use(bodyParser.json({limit: "50mb"}));
 
 app.use(function (req, res, next) {
 
